fe001ef4e5e75abac76ead82ac960749f2f88f90
emanuele <me.emanuele.xyz@gmail.com>
2023-10-18 22:58:50 +0200

    add battling through the lands project

---
+++ BtL/src/Main.cpp
@@ -0,0 +1,8 @@
+#include <BtL\pch.h>
+#include <BtL\BtL.h>
+
+int main()
+{
+	std::cout << "Hello from Battling Through the Lands!" << std::endl;
+	return 0;
+}
---
+++ BtL/src/pch.cpp
@@ -0,0 +1,1 @@
+#include <BtL/pch.h>
