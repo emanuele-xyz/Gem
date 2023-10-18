23d71ba4b1f99c727785d816357750ab262c6e65
emanuele <me.emanuele.xyz@gmail.com>
2023-10-18 23:21:41 +0200

    integrate Gem into BtL

--- BtL/src/Main.cpp
+++ BtL/src/Main.cpp
@@ -1,8 +1,10 @@
 #include <BtL\pch.h>
 #include <BtL\BtL.h>
+#include <Gem\Gem.h>
 
 int main()
 {
 	std::cout << "Hello from Battling Through the Lands!" << std::endl;
+	Gem::Hello();
 	return 0;
 }
