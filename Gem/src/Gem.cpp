41d131c2a5d34050507bf660b359a9da53e8af16
emanuele <me.emanuele.xyz@gmail.com>
2023-10-22 16:38:34 +0200

    add spdlog dependency

--- Gem/src/Gem.cpp
+++ Gem/src/Gem.cpp
@@ -1,6 +1,10 @@
 #include <Gem\pch.h>
 #include <Gem\Gem.h>
 #include <taskflow\taskflow.hpp>
+#include <spdlog\spdlog.h>
+#include <spdlog\async.h>
+#include <spdlog\sinks\basic_file_sink.h>
+#include <spdlog\sinks\stdout_color_sinks.h>
 
 namespace Gem
 {
@@ -8,12 +12,16 @@ namespace Gem
 	{
 		std::cout << "Hello from Gem!" << std::endl;
 
-		auto n_threads{ std::thread::hardware_concurrency() - 1 };
+		spdlog::init_thread_pool(2048, 1);
+		auto console_logger{ spdlog::stdout_color_mt<spdlog::async_factory>("console") };
+		console_logger->info("Henlo!");
+
+		auto n_threads{ std::thread::hardware_concurrency() - 2 };
 		tf::Executor executor{ n_threads };
 		tf::Taskflow taskflow{};
 
-		auto A{ taskflow.emplace([]() { std::cout << "TaskA" << std::endl; }) };
-		auto B{ taskflow.emplace([]() { std::cout << "TaskB" << std::endl; }) };
+		taskflow.emplace([]() { std::cout << "TaskA\n"; });
+		taskflow.emplace([]() { std::cout << "TaskB\n"; });
 
 		executor.run(taskflow).wait();
 	}
