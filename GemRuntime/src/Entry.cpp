2a6198c0a6cd9d8f7498f1e9230ee1977f064564
emanuele <me.emanuele.xyz@gmail.com>
2023-10-26 23:04:47 +0200

    import Gem into GemRuntime

--- GemRuntime/src/Entry.cpp
+++ GemRuntime/src/Entry.cpp
@@ -1,9 +1,11 @@
 #include <GemRuntime\Entry.h>
+#include <Gem\Gem.h>
 
 namespace GemRuntime
 {
 	void Entry()
 	{
+		Gem::ShowErrorPopup("Hello from Gem Runtime!");
 		// TODO: to be implemented
 		// TODO: create and run runtime application
 	}
